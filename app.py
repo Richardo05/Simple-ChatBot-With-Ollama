e60045877083f397f40831881bf5e642bd6f032a
Richardo Kaka Widjaja <105096180+Richardo05@users.noreply.github.com>
2025-11-18 19:27:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,12 @@ from flask import Flask, render_template, request, jsonify
 from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
 import langchain
-from langchain.chains import LLMChain
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.pydantic_v1 import BaseModel, Field
-import requests
+
 
 app = Flask(__name__)
 
-# class Answer(BaseModel):
-#     answer: str = Field(description='Answer to the question')
-
 try:
     model = Ollama(model='gemma3:1b')
-
-    # output_parser = JsonOutputParser(pydantic_object=Answer)
 
     template = """
         You are a helpful and concise AI assistant. Answer the user's question clearly.
@@ -25,11 +17,6 @@ try:
         ANSWER: 
     """
 
-    # prompt = PromptTemplate(
-    #     template=template,
-    #     input_variables=["question"],
-    #     partial_variables={"format_instructions": output_parser.get_format_instructions()},
-    # )
     prompt = PromptTemplate.from_template(template)
 
     chain = prompt | model
@@ -44,7 +31,7 @@ except Exception as e:
 
 @app.route('/')
 def home():
-    return render_template('index1.html')
+    return render_template('index.html')
 
 @app.route('/inference', methods=['POST'])
 def ineference():
@@ -59,4 +46,5 @@ def ineference():
 if __name__ == '__main__':
     print("Starting Ollama Flask application...")
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
